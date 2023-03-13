e573921c268d52cdbbcd17a613b1695bec10bf42
cutehedgehog <yulia.atrashonok@yandex.ru>
2023-03-14 01:13:11 +0300

    Added second task

--- lab1.py
+++ lab1.py
@@ -1,1 +1,26 @@
 print("Hello world!")
+
+ADD = "add"
+SUB = "sub"
+MUL = "mul"
+DIV = "div"
+
+
+def math_operations(num1, num2, op):
+    if op == ADD:
+        return num1 + num2
+    elif op == SUB:
+        return num1 - num2
+    elif op == MUL:
+        return num1 * num2
+    elif op == DIV:
+        return num1 / num2
+    else:
+        return None
+
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+operation = input("Enter operation: ")
+
+print("Result: ", math_operations(number1, number2, operation))
